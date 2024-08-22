oracle/graal

svm: adopt "JDK-8328366: Thread.setContextClassloader from thread in FJP commonPool task no longer works after JDK-8327501"

substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/RecomputedFields.java
@@ -33,6 +33,7 @@ import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.nio.charset.CharsetDecoder;
+import java.security.AccessControlContext;
 import java.util.concurrent.ForkJoinPool;
 
 import org.graalvm.nativeimage.ImageSingletons;
@@ -308,6 +309,15 @@ class ForkJoinPoolCommonAccessor {
         }
         return result;
     }
+}
+
+@TargetClass(value = java.util.concurrent.ForkJoinPool.class, innerClass = "DefaultForkJoinWorkerThreadFactory", onlyWith = JDK23OrLater.class)
+@SuppressWarnings("removal")
+final class Target_java_util_concurrent_ForkJoinPool_DefaultForkJoinWorkerThreadFactory {
+    @Alias @RecomputeFieldValue(kind = Reset) //
+    static AccessControlContext regularACC;
+    @Alias @RecomputeFieldValue(kind = Reset) //
+    static AccessControlContext commonACC;
 }
 
 /** Dummy class to have a class with the file's name. */

graalvm/graalvm-community-jdk21u

(cherry picked from commit 10bb0c6797753cd21ceef3bd2228f66dc73969e0)

substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/jdk/RecomputedFields.java
@@ -33,6 +33,7 @@ import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.nio.charset.CharsetDecoder;
+import java.security.AccessControlContext;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ForkJoinPool;
@@ -384,6 +385,15 @@ class ForkJoinPoolCommonAccessor {
         }
         return result;
     }
+}
+
+@TargetClass(value = java.util.concurrent.ForkJoinPool.class, innerClass = "DefaultForkJoinWorkerThreadFactory", onlyWith = JDK21OrLater.class)
+@SuppressWarnings("removal")
+final class Target_java_util_concurrent_ForkJoinPool_DefaultForkJoinWorkerThreadFactory {
+    @Alias @RecomputeFieldValue(kind = Reset) //
+    static AccessControlContext regularACC;
+    @Alias @RecomputeFieldValue(kind = Reset) //
+    static AccessControlContext commonACC;
 }
 
 /** Dummy class to have a class with the file's name. */