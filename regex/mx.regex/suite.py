1dcdec87987c960a14ea02ab276e747820a962c7
Foivos Zakkak <fzakkak@redhat.com>
2025-01-22 20:30:14 +0200

    New dev cycle GraalVM Community 23.1.7

--- regex/mx.regex/suite.py
+++ regex/mx.regex/suite.py
@@ -43,8 +43,8 @@ suite = {
 
   "name" : "regex",
 
-  "version" : "23.1.6",
-  "release" : True,
+  "version" : "23.1.7",
+  "release" : False,
   "groupId" : "org.graalvm.regex",
   "url" : "http://www.graalvm.org/",
   "developer" : {
--- wasm/mx.wasm/suite.py
+++ wasm/mx.wasm/suite.py
@@ -42,7 +42,7 @@ suite = {
   "mxversion": "6.41.0",
   "name" : "wasm",
   "groupId" : "org.graalvm.wasm",
-  "version" : "23.1.6",
+  "version" : "23.1.7",
   "versionConflictResolution" : "latest",
   "url" : "http://graalvm.org/",
   "developer" : {
