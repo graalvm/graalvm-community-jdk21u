oracle/graal

Use correct compare size in AMD64NormalizedUnsignedCompareOp

compiler/src/jdk.graal.compiler.test/src/jdk/graal/compiler/replacements/test/NarrowCompareUnsignedTest.java
@@ -1,0 +1,182 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package jdk.graal.compiler.replacements.test;
+
+import org.junit.Assume;
+import org.junit.Test;
+
+import jdk.graal.compiler.core.test.GraalCompilerTest;
+import jdk.graal.compiler.nodes.ConstantNode;
+import jdk.graal.compiler.nodes.NodeView;
+import jdk.graal.compiler.nodes.ValueNode;
+import jdk.graal.compiler.nodes.calc.AndNode;
+import jdk.graal.compiler.nodes.calc.IntegerNormalizeCompareNode;
+import jdk.graal.compiler.nodes.calc.NarrowNode;
+import jdk.graal.compiler.nodes.graphbuilderconf.GraphBuilderContext;
+import jdk.graal.compiler.nodes.graphbuilderconf.InvocationPlugin;
+import jdk.graal.compiler.nodes.graphbuilderconf.InvocationPlugins;
+import jdk.vm.ci.meta.JavaKind;
+import jdk.vm.ci.meta.ResolvedJavaMethod;
+
+/**
+ * Tests for {@link IntegerNormalizeCompareNode} applied to subword integers.
+ */
+public class NarrowCompareUnsignedTest extends GraalCompilerTest {
+
+    public static int narrowCompareUnsignedByte(int x, int y) {
+        /*
+         * This expression doesn't quite produce the graph shape we want: The narrows feed into sign
+         * extensions that feed into the actual compare. We want to test the compare when applied to
+         * the actual narrows. It's possible but fairly difficult to produce that graph shape in
+         * practice. Therefore we intrinsify this method to get what we want.
+         */
+        return Integer.compareUnsigned((byte) x, (byte) y);
+    }
+
+    public static int narrowCompareUnsignedShort(int x, int y) {
+        return Integer.compareUnsigned((short) x, (short) y);
+    }
+
+    public static int narrowCompareUnsignedChar(int x, int y) {
+        return Integer.compareUnsigned((char) x, (char) y);
+    }
+
+    @Override
+    protected void registerInvocationPlugins(InvocationPlugins invocationPlugins) {
+        InvocationPlugins.Registration r = new InvocationPlugins.Registration(invocationPlugins, NarrowCompareUnsignedTest.class);
+        r.register(new InvocationPlugin.InlineOnlyInvocationPlugin("narrowCompareUnsignedByte", int.class, int.class) {
+            @Override
+            public boolean apply(GraphBuilderContext b, ResolvedJavaMethod targetMethod, Receiver receiver, ValueNode x, ValueNode y) {
+                ValueNode narrowX = NarrowNode.create(x, Byte.SIZE, NodeView.DEFAULT);
+                ValueNode narrowY = NarrowNode.create(y, Byte.SIZE, NodeView.DEFAULT);
+                b.addPush(JavaKind.Int, IntegerNormalizeCompareNode.create(narrowX, narrowY, true, JavaKind.Int, b.getConstantReflection()));
+                return true;
+            }
+        });
+        r.register(new InvocationPlugin.InlineOnlyInvocationPlugin("narrowCompareUnsignedShort", int.class, int.class) {
+            @Override
+            public boolean apply(GraphBuilderContext b, ResolvedJavaMethod targetMethod, Receiver receiver, ValueNode x, ValueNode y) {
+                ValueNode narrowX = NarrowNode.create(x, Short.SIZE, NodeView.DEFAULT);
+                ValueNode narrowY = NarrowNode.create(y, Short.SIZE, NodeView.DEFAULT);
+                b.addPush(JavaKind.Int, IntegerNormalizeCompareNode.create(narrowX, narrowY, true, JavaKind.Int, b.getConstantReflection()));
+                return true;
+            }
+        });
+        r.register(new InvocationPlugin.InlineOnlyInvocationPlugin("narrowCompareUnsignedChar", int.class, int.class) {
+            @Override
+            public boolean apply(GraphBuilderContext b, ResolvedJavaMethod targetMethod, Receiver receiver, ValueNode x, ValueNode y) {
+                int unsignedMask = 0xffff;
+                ValueNode maskedX = AndNode.create(x, ConstantNode.forInt(unsignedMask), NodeView.DEFAULT);
+                ValueNode maskedY = AndNode.create(y, ConstantNode.forInt(unsignedMask), NodeView.DEFAULT);
+                ValueNode narrowX = NarrowNode.create(maskedX, Character.SIZE, NodeView.DEFAULT);
+                ValueNode narrowY = NarrowNode.create(maskedY, Character.SIZE, NodeView.DEFAULT);
+                b.addPush(JavaKind.Int, IntegerNormalizeCompareNode.create(narrowX, narrowY, true, JavaKind.Int, b.getConstantReflection()));
+                return true;
+            }
+        });
+        super.registerInvocationPlugins(invocationPlugins);
+    }
+
+    private static final int[] TEST_VALUES = new int[]{0, 40, Byte.MAX_VALUE, Byte.MAX_VALUE + 40, Short.MAX_VALUE, Short.MAX_VALUE + 40, Character.MAX_VALUE, Character.MAX_VALUE + 40};
+
+    public static int byteVariableSnippet(int x, int y) {
+        return narrowCompareUnsignedByte(x, y);
+    }
+
+    @Test
+    public void byteVariable() {
+        Assume.assumeTrue("only test byte compare when supported on target", getLowerer().smallestCompareWidth() <= Byte.SIZE);
+        for (int x : TEST_VALUES) {
+            for (int y : TEST_VALUES) {
+                test("byteVariableSnippet", x, y);
+            }
+        }
+    }
+
+    public static int byteConstantSnippet(int x) {
+        return narrowCompareUnsignedByte(x, Byte.MAX_VALUE);
+    }
+
+    @Test
+    public void byteConstant() {
+        Assume.assumeTrue("only test byte compare when supported on target", getLowerer().smallestCompareWidth() <= Byte.SIZE);
+        for (int x : TEST_VALUES) {
+            test("byteConstantSnippet", x);
+        }
+    }
+
+    public static int shortVariableSnippet(int x, int y) {
+        return narrowCompareUnsignedShort(x, y);
+    }
+
+    @Test
+    public void shortVariable() {
+        Assume.assumeTrue("only test short compare when supported on target", getLowerer().smallestCompareWidth() <= Short.SIZE);
+        for (int x : TEST_VALUES) {
+            for (int y : TEST_VALUES) {
+                test("shortVariableSnippet", x, y);
+            }
+        }
+    }
+
+    public static int shortConstantSnippet(int x) {
+        return narrowCompareUnsignedShort(x, Short.MAX_VALUE);
+    }
+
+    @Test
+    public void shortConstant() {
+        Assume.assumeTrue("only test short compare when supported on target", getLowerer().smallestCompareWidth() <= Short.SIZE);
+        for (int x : TEST_VALUES) {
+            test("shortConstantSnippet", x);
+        }
+    }
+
+    public static int charVariableSnippet(int x, int y) {
+        return narrowCompareUnsignedChar(x, y);
+    }
+
+    @Test
+    public void charVariable() {
+        Assume.assumeTrue("only test char compare when supported on target", getLowerer().smallestCompareWidth() <= Character.SIZE);
+        for (int x : TEST_VALUES) {
+            for (int y : TEST_VALUES) {
+                test("charVariableSnippet", x, y);
+            }
+        }
+    }
+
+    public static int charConstantSnippet(int x) {
+        return narrowCompareUnsignedChar(x, Character.MAX_VALUE);
+    }
+
+    @Test
+    public void charConstant() {
+        Assume.assumeTrue("only test char compare when supported on target", getLowerer().smallestCompareWidth() <= Character.SIZE);
+        for (int x : TEST_VALUES) {
+            test("charConstantSnippet", x);
+        }
+    }
+}

compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/core/amd64/AMD64NodeMatchRules.java
@@ -56,6 +56,7 @@ import jdk.graal.compiler.core.common.calc.Condition;
 import jdk.graal.compiler.core.common.calc.FloatConvertCategory;
 import jdk.graal.compiler.core.common.memory.MemoryExtendKind;
 import jdk.graal.compiler.core.common.memory.MemoryOrderMode;
+import jdk.graal.compiler.core.common.type.PrimitiveStamp;
 import jdk.graal.compiler.core.gen.NodeLIRBuilder;
 import jdk.graal.compiler.core.gen.NodeMatchRules;
 import jdk.graal.compiler.core.match.ComplexMatchResult;
@@ -738,7 +739,11 @@ public class AMD64NodeMatchRules extends NodeMatchRules {
     public ComplexMatchResult normalizedIntegerCompare(ValueNode x, ValueNode y, ConstantNode cm1, ConstantNode c0, ConstantNode c1) {
         if (cm1.getStackKind() == JavaKind.Int && cm1.asJavaConstant().asInt() == -1 && c0.getStackKind() == JavaKind.Int && c0.asJavaConstant().asInt() == 0 && c1.getStackKind() == JavaKind.Int &&
                         c1.asJavaConstant().asInt() == 1) {
-            return builder -> getArithmeticLIRGenerator().emitNormalizedUnsignedCompare(operand(x), operand(y));
+            GraalError.guarantee(PrimitiveStamp.getBits(x.stamp(NodeView.DEFAULT)) == PrimitiveStamp.getBits(y.stamp(NodeView.DEFAULT)), "need compatible inputs: %s, %s", x, y);
+            return builder -> {
+                LIRKind compareKind = gen.getLIRKind(x.stamp(NodeView.DEFAULT));
+                return getArithmeticLIRGenerator().emitNormalizedUnsignedCompare(compareKind, operand(x), operand(y));
+            };
         }
         return null;
     }

compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/lir/amd64/AMD64NormalizedUnsignedCompareOp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,17 +24,17 @@
  */
 package jdk.graal.compiler.lir.amd64;
 
+import static jdk.graal.compiler.asm.amd64.AMD64Assembler.AMD64BinaryArithmetic.CMP;
 import static jdk.vm.ci.code.ValueUtil.asRegister;
 
 import jdk.graal.compiler.asm.Label;
 import jdk.graal.compiler.asm.amd64.AMD64Assembler;
+import jdk.graal.compiler.asm.amd64.AMD64BaseAssembler;
 import jdk.graal.compiler.asm.amd64.AMD64MacroAssembler;
-import jdk.graal.compiler.debug.GraalError;
+import jdk.graal.compiler.core.common.LIRKind;
 import jdk.graal.compiler.lir.LIRInstructionClass;
 import jdk.graal.compiler.lir.SyncPort;
 import jdk.graal.compiler.lir.asm.CompilationResultBuilder;
-
-import jdk.vm.ci.amd64.AMD64Kind;
 import jdk.vm.ci.meta.AllocatableValue;
 
 /**
@@ -53,23 +53,22 @@ public class AMD64NormalizedUnsignedCompareOp extends AMD64LIRInstruction {
     @Use({OperandFlag.REG}) protected AllocatableValue x;
     @Use({OperandFlag.REG}) protected AllocatableValue y;
 
-    public AMD64NormalizedUnsignedCompareOp(AllocatableValue result, AllocatableValue x, AllocatableValue y) {
+    private final LIRKind compareKind;
+
+    public AMD64NormalizedUnsignedCompareOp(AllocatableValue result, LIRKind compareKind, AllocatableValue x, AllocatableValue y) {
         super(TYPE);
 
         this.result = result;
         this.x = x;
         this.y = y;
+        this.compareKind = compareKind;
     }
 
     @Override
     public void emitCode(CompilationResultBuilder crb, AMD64MacroAssembler masm) {
         Label done = new Label();
-        if (x.getPlatformKind() == AMD64Kind.DWORD) {
-            masm.cmpl(asRegister(x), asRegister(y));
-        } else {
-            GraalError.guarantee(x.getPlatformKind() == AMD64Kind.QWORD, "unsupported value kind %s", x.getPlatformKind());
-            masm.cmpq(asRegister(x), asRegister(y));
-        }
+        AMD64BaseAssembler.OperandSize size = AMD64BaseAssembler.OperandSize.get(compareKind.getPlatformKind());
+        CMP.getRMOpcode(size).emit(masm, size, asRegister(x), asRegister(y));
         masm.movl(asRegister(result), -1);
         masm.jccb(AMD64Assembler.ConditionFlag.Below, done);
         masm.setl(AMD64Assembler.ConditionFlag.NotEqual, asRegister(result));

graalvm/graalvm-community-jdk21u

(cherry picked from commit 2a2d7bbd7d0aefef6595b0bc5d661d8b9640ea7f)

compiler/src/jdk.internal.vm.compiler.test/src/org/graalvm/compiler/replacements/test/NarrowCompareUnsignedTest.java
@@ -1,0 +1,182 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package org.graalvm.compiler.replacements.test;
+
+import org.junit.Assume;
+import org.junit.Test;
+
+import org.graalvm.compiler.core.test.GraalCompilerTest;
+import org.graalvm.compiler.nodes.ConstantNode;
+import org.graalvm.compiler.nodes.NodeView;
+import org.graalvm.compiler.nodes.ValueNode;
+import org.graalvm.compiler.nodes.calc.AndNode;
+import org.graalvm.compiler.nodes.calc.IntegerNormalizeCompareNode;
+import org.graalvm.compiler.nodes.calc.NarrowNode;
+import org.graalvm.compiler.nodes.graphbuilderconf.GraphBuilderContext;
+import org.graalvm.compiler.nodes.graphbuilderconf.InvocationPlugin;
+import org.graalvm.compiler.nodes.graphbuilderconf.InvocationPlugins;
+import jdk.vm.ci.meta.JavaKind;
+import jdk.vm.ci.meta.ResolvedJavaMethod;
+
+/**
+ * Tests for {@link IntegerNormalizeCompareNode} applied to subword integers.
+ */
+public class NarrowCompareUnsignedTest extends GraalCompilerTest {
+
+    public static int narrowCompareUnsignedByte(int x, int y) {
+        /*
+         * This expression doesn't quite produce the graph shape we want: The narrows feed into sign
+         * extensions that feed into the actual compare. We want to test the compare when applied to
+         * the actual narrows. It's possible but fairly difficult to produce that graph shape in
+         * practice. Therefore we intrinsify this method to get what we want.
+         */
+        return Integer.compareUnsigned((byte) x, (byte) y);
+    }
+
+    public static int narrowCompareUnsignedShort(int x, int y) {
+        return Integer.compareUnsigned((short) x, (short) y);
+    }
+
+    public static int narrowCompareUnsignedChar(int x, int y) {
+        return Integer.compareUnsigned((char) x, (char) y);
+    }
+
+    @Override
+    protected void registerInvocationPlugins(InvocationPlugins invocationPlugins) {
+        InvocationPlugins.Registration r = new InvocationPlugins.Registration(invocationPlugins, NarrowCompareUnsignedTest.class);
+        r.register(new InvocationPlugin.InlineOnlyInvocationPlugin("narrowCompareUnsignedByte", int.class, int.class) {
+            @Override
+            public boolean apply(GraphBuilderContext b, ResolvedJavaMethod targetMethod, Receiver receiver, ValueNode x, ValueNode y) {
+                ValueNode narrowX = NarrowNode.create(x, Byte.SIZE, NodeView.DEFAULT);
+                ValueNode narrowY = NarrowNode.create(y, Byte.SIZE, NodeView.DEFAULT);
+                b.addPush(JavaKind.Int, IntegerNormalizeCompareNode.create(narrowX, narrowY, true, JavaKind.Int, b.getConstantReflection()));
+                return true;
+            }
+        });
+        r.register(new InvocationPlugin.InlineOnlyInvocationPlugin("narrowCompareUnsignedShort", int.class, int.class) {
+            @Override
+            public boolean apply(GraphBuilderContext b, ResolvedJavaMethod targetMethod, Receiver receiver, ValueNode x, ValueNode y) {
+                ValueNode narrowX = NarrowNode.create(x, Short.SIZE, NodeView.DEFAULT);
+                ValueNode narrowY = NarrowNode.create(y, Short.SIZE, NodeView.DEFAULT);
+                b.addPush(JavaKind.Int, IntegerNormalizeCompareNode.create(narrowX, narrowY, true, JavaKind.Int, b.getConstantReflection()));
+                return true;
+            }
+        });
+        r.register(new InvocationPlugin.InlineOnlyInvocationPlugin("narrowCompareUnsignedChar", int.class, int.class) {
+            @Override
+            public boolean apply(GraphBuilderContext b, ResolvedJavaMethod targetMethod, Receiver receiver, ValueNode x, ValueNode y) {
+                int unsignedMask = 0xffff;
+                ValueNode maskedX = AndNode.create(x, ConstantNode.forInt(unsignedMask), NodeView.DEFAULT);
+                ValueNode maskedY = AndNode.create(y, ConstantNode.forInt(unsignedMask), NodeView.DEFAULT);
+                ValueNode narrowX = NarrowNode.create(maskedX, Character.SIZE, NodeView.DEFAULT);
+                ValueNode narrowY = NarrowNode.create(maskedY, Character.SIZE, NodeView.DEFAULT);
+                b.addPush(JavaKind.Int, IntegerNormalizeCompareNode.create(narrowX, narrowY, true, JavaKind.Int, b.getConstantReflection()));
+                return true;
+            }
+        });
+        super.registerInvocationPlugins(invocationPlugins);
+    }
+
+    private static final int[] TEST_VALUES = new int[]{0, 40, Byte.MAX_VALUE, Byte.MAX_VALUE + 40, Short.MAX_VALUE, Short.MAX_VALUE + 40, Character.MAX_VALUE, Character.MAX_VALUE + 40};
+
+    public static int byteVariableSnippet(int x, int y) {
+        return narrowCompareUnsignedByte(x, y);
+    }
+
+    @Test
+    public void byteVariable() {
+        Assume.assumeTrue("only test byte compare when supported on target", getLowerer().smallestCompareWidth() <= Byte.SIZE);
+        for (int x : TEST_VALUES) {
+            for (int y : TEST_VALUES) {
+                test("byteVariableSnippet", x, y);
+            }
+        }
+    }
+
+    public static int byteConstantSnippet(int x) {
+        return narrowCompareUnsignedByte(x, Byte.MAX_VALUE);
+    }
+
+    @Test
+    public void byteConstant() {
+        Assume.assumeTrue("only test byte compare when supported on target", getLowerer().smallestCompareWidth() <= Byte.SIZE);
+        for (int x : TEST_VALUES) {
+            test("byteConstantSnippet", x);
+        }
+    }
+
+    public static int shortVariableSnippet(int x, int y) {
+        return narrowCompareUnsignedShort(x, y);
+    }
+
+    @Test
+    public void shortVariable() {
+        Assume.assumeTrue("only test short compare when supported on target", getLowerer().smallestCompareWidth() <= Short.SIZE);
+        for (int x : TEST_VALUES) {
+            for (int y : TEST_VALUES) {
+                test("shortVariableSnippet", x, y);
+            }
+        }
+    }
+
+    public static int shortConstantSnippet(int x) {
+        return narrowCompareUnsignedShort(x, Short.MAX_VALUE);
+    }
+
+    @Test
+    public void shortConstant() {
+        Assume.assumeTrue("only test short compare when supported on target", getLowerer().smallestCompareWidth() <= Short.SIZE);
+        for (int x : TEST_VALUES) {
+            test("shortConstantSnippet", x);
+        }
+    }
+
+    public static int charVariableSnippet(int x, int y) {
+        return narrowCompareUnsignedChar(x, y);
+    }
+
+    @Test
+    public void charVariable() {
+        Assume.assumeTrue("only test char compare when supported on target", getLowerer().smallestCompareWidth() <= Character.SIZE);
+        for (int x : TEST_VALUES) {
+            for (int y : TEST_VALUES) {
+                test("charVariableSnippet", x, y);
+            }
+        }
+    }
+
+    public static int charConstantSnippet(int x) {
+        return narrowCompareUnsignedChar(x, Character.MAX_VALUE);
+    }
+
+    @Test
+    public void charConstant() {
+        Assume.assumeTrue("only test char compare when supported on target", getLowerer().smallestCompareWidth() <= Character.SIZE);
+        for (int x : TEST_VALUES) {
+            test("charConstantSnippet", x);
+        }
+    }
+}

compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/core/amd64/AMD64NodeMatchRules.java
@@ -55,6 +55,7 @@ import org.graalvm.compiler.core.common.calc.CanonicalCondition;
 import org.graalvm.compiler.core.common.calc.Condition;
 import org.graalvm.compiler.core.common.memory.MemoryExtendKind;
 import org.graalvm.compiler.core.common.memory.MemoryOrderMode;
+import org.graalvm.compiler.core.common.type.PrimitiveStamp;
 import org.graalvm.compiler.core.gen.NodeLIRBuilder;
 import org.graalvm.compiler.core.gen.NodeMatchRules;
 import org.graalvm.compiler.core.match.ComplexMatchResult;
@@ -729,7 +730,11 @@ public class AMD64NodeMatchRules extends NodeMatchRules {
     public ComplexMatchResult normalizedIntegerCompare(ValueNode x, ValueNode y, ConstantNode cm1, ConstantNode c0, ConstantNode c1) {
         if (cm1.getStackKind() == JavaKind.Int && cm1.asJavaConstant().asInt() == -1 && c0.getStackKind() == JavaKind.Int && c0.asJavaConstant().asInt() == 0 && c1.getStackKind() == JavaKind.Int &&
                         c1.asJavaConstant().asInt() == 1) {
-            return builder -> getArithmeticLIRGenerator().emitNormalizedUnsignedCompare(operand(x), operand(y));
+            GraalError.guarantee(PrimitiveStamp.getBits(x.stamp(NodeView.DEFAULT)) == PrimitiveStamp.getBits(y.stamp(NodeView.DEFAULT)), "need compatible inputs: %s, %s", x, y);
+            return builder -> {
+                LIRKind compareKind = gen.getLIRKind(x.stamp(NodeView.DEFAULT));
+                return getArithmeticLIRGenerator().emitNormalizedUnsignedCompare(compareKind, operand(x), operand(y));
+            };
         }
         return null;
     }

compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/lir/amd64/AMD64NormalizedUnsignedCompareOp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,17 +25,18 @@
 package org.graalvm.compiler.lir.amd64;
 
 import static jdk.vm.ci.code.ValueUtil.asRegister;
+import static org.graalvm.compiler.asm.amd64.AMD64Assembler.AMD64BinaryArithmetic.CMP;
 import static org.graalvm.compiler.lir.LIRInstruction.OperandFlag.REG;
 
 import org.graalvm.compiler.asm.Label;
 import org.graalvm.compiler.asm.amd64.AMD64Assembler;
+import org.graalvm.compiler.asm.amd64.AMD64BaseAssembler;
 import org.graalvm.compiler.asm.amd64.AMD64MacroAssembler;
-import org.graalvm.compiler.debug.GraalError;
+import org.graalvm.compiler.core.common.LIRKind;
 import org.graalvm.compiler.lir.LIRInstructionClass;
 import org.graalvm.compiler.lir.SyncPort;
 import org.graalvm.compiler.lir.asm.CompilationResultBuilder;
 
-import jdk.vm.ci.amd64.AMD64Kind;
 import jdk.vm.ci.meta.AllocatableValue;
 
 /**
@@ -54,23 +55,22 @@ public class AMD64NormalizedUnsignedCompareOp extends AMD64LIRInstruction {
     @Use({REG}) protected AllocatableValue x;
     @Use({REG}) protected AllocatableValue y;
 
-    public AMD64NormalizedUnsignedCompareOp(AllocatableValue result, AllocatableValue x, AllocatableValue y) {
+    private final LIRKind compareKind;
+
+    public AMD64NormalizedUnsignedCompareOp(AllocatableValue result, LIRKind compareKind, AllocatableValue x, AllocatableValue y) {
         super(TYPE);
 
         this.result = result;
         this.x = x;
         this.y = y;
+        this.compareKind = compareKind;
     }
 
     @Override
     public void emitCode(CompilationResultBuilder crb, AMD64MacroAssembler masm) {
         Label done = new Label();
-        if (x.getPlatformKind() == AMD64Kind.DWORD) {
-            masm.cmpl(asRegister(x), asRegister(y));
-        } else {
-            GraalError.guarantee(x.getPlatformKind() == AMD64Kind.QWORD, "unsupported value kind %s", x.getPlatformKind());
-            masm.cmpq(asRegister(x), asRegister(y));
-        }
+        AMD64BaseAssembler.OperandSize size = AMD64BaseAssembler.OperandSize.get(compareKind.getPlatformKind());
+        CMP.getRMOpcode(size).emit(masm, size, asRegister(x), asRegister(y));
         masm.movl(asRegister(result), -1);
         masm.jccb(AMD64Assembler.ConditionFlag.Below, done);
         masm.setl(AMD64Assembler.ConditionFlag.NotEqual, asRegister(result));