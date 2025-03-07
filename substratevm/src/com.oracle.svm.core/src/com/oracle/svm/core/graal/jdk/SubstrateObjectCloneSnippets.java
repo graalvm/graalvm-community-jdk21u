bc7ab0a36f5ca2affd9508fd464f5568b10decc5
Christian Haeubl <christian.haeubl@oracle.com>
2025-03-07 18:35:24 +0100

    Throw a CloneNotSupportedException for java.lang.ref.Reference and its subclasses.
    (cherry picked from commit 4325cfe21cde28a9d49218b11e17b0902f195b23)

--- substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/graal/jdk/SubstrateObjectCloneSnippets.java
+++ substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/graal/jdk/SubstrateObjectCloneSnippets.java
@@ -68,6 +68,7 @@ import com.oracle.svm.core.heap.Pod;
 import com.oracle.svm.core.heap.PodReferenceMapDecoder;
 import com.oracle.svm.core.hub.DynamicHub;
 import com.oracle.svm.core.hub.DynamicHubSupport;
+import com.oracle.svm.core.hub.HubType;
 import com.oracle.svm.core.hub.LayoutEncoding;
 import com.oracle.svm.core.meta.SharedType;
 import com.oracle.svm.core.snippets.KnownIntrinsics;
@@ -90,14 +91,19 @@ public final class SubstrateObjectCloneSnippets extends SubstrateTemplates imple
     }
 
     @SubstrateForeignCallTarget(stubCallingConvention = false)
+    // @BasedOnJDKFile("https://github.com/openjdk/jdk/blob/jdk-21+35/src/hotspot/share/prims/jvm.cpp#L643-L692")
     private static Object doClone(Object original) throws CloneNotSupportedException, InstantiationException {
         if (original == null) {
             throw new NullPointerException();
         } else if (!(original instanceof Cloneable)) {
-            throw new CloneNotSupportedException("Object is no instance of Cloneable.");
+            throw new CloneNotSupportedException("Object is no instance of Cloneable: " + original.getClass().getName());
         }
 
         DynamicHub hub = KnownIntrinsics.readHub(original);
+        if (hub.getHubType() == HubType.REFERENCE_INSTANCE) {
+            throw new CloneNotSupportedException("Subclasses of java.lang.ref.Reference are not cloneable: " + hub.getName());
+        }
+
         int layoutEncoding = hub.getLayoutEncoding();
         boolean isArrayLike = LayoutEncoding.isArrayLike(layoutEncoding);
 
