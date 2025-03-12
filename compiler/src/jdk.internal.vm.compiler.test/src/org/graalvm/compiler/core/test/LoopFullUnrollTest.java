oracle/graal

loop infra: use correct bit size for overflow checks

compiler/src/jdk.graal.compiler.test/src/jdk/graal/compiler/core/test/LoopFullUnrollTest.java
@@ -24,6 +24,7 @@
  */
 package jdk.graal.compiler.core.test;
 
+import jdk.graal.compiler.api.directives.GraalDirectives;
 import jdk.graal.compiler.debug.DebugContext;
 import jdk.graal.compiler.debug.DebugDumpScope;
 import jdk.graal.compiler.loop.phases.LoopFullUnrollPhase;
@@ -94,5 +95,39 @@ public class LoopFullUnrollTest extends GraalCompilerTest {
         } catch (Throwable e) {
             throw debug.handle(e);
         }
+    }
+
+    public static int snippetFlows() {
+        int init = Integer.MIN_VALUE;
+        int step = -1;
+        int limit = 1;
+        int phi = init;
+        while (Integer.MIN_VALUE - phi < limit) {
+            GraalDirectives.sideEffect();
+            phi = phi + step;
+        }
+        return phi;
+    }
+
+    @Test
+    public void testFlows() {
+        test("snippetFlows");
+    }
+
+    public static int snippetFlows2() {
+        int init = Integer.MAX_VALUE;
+        int step = -8;
+        int limit = 8184;
+        int phi = init;
+        while (Integer.MIN_VALUE - phi < limit) {
+            GraalDirectives.sideEffect();
+            phi = phi + step;
+        }
+        return phi;
+    }
+
+    @Test
+    public void testFlows2() {
+        test("snippetFlows2");
     }
 }

compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/loop/DerivedOffsetInductionVariable.java
@@ -24,6 +24,7 @@
  */
 package jdk.graal.compiler.nodes.loop;
 
+import jdk.graal.compiler.core.common.type.IntegerStamp;
 import jdk.graal.compiler.core.common.type.Stamp;
 import jdk.graal.compiler.debug.Assertions;
 import jdk.graal.compiler.debug.GraalError;
@@ -34,6 +35,7 @@ import jdk.graal.compiler.nodes.calc.BinaryArithmeticNode;
 import jdk.graal.compiler.nodes.calc.IntegerConvertNode;
 import jdk.graal.compiler.nodes.calc.NegateNode;
 import jdk.graal.compiler.nodes.calc.SubNode;
+import jdk.graal.compiler.phases.common.util.LoopUtility;
 import jdk.graal.compiler.replacements.nodes.arithmetic.IntegerExactArithmeticNode;
 
 public class DerivedOffsetInductionVariable extends DerivedInductionVariable {
@@ -102,7 +104,7 @@ public class DerivedOffsetInductionVariable extends DerivedInductionVariable {
 
     private long constantStrideSafe() throws ArithmeticException {
         if (value instanceof SubNode && base.valueNode() == value.getY()) {
-            return Math.multiplyExact(base.constantStride(), -1);
+            return LoopUtility.multiplyExact(IntegerStamp.getBits(offset.stamp(NodeView.DEFAULT)), base.constantStride(), -1);
         }
         return base.constantStride();
     }
@@ -158,16 +160,18 @@ public class DerivedOffsetInductionVariable extends DerivedInductionVariable {
     }
 
     private long opSafe(long b, long o) throws ArithmeticException {
+        // we can use offset bits in this method because all operands (init, scale, stride and
+        // extremum) have by construction equal bit sizes
         if (value instanceof AddNode) {
-            return Math.addExact(b, o);
+            return LoopUtility.addExact(IntegerStamp.getBits(offset.stamp(NodeView.DEFAULT)), b, o);
         }
         if (value instanceof SubNode) {
             if (base.valueNode() == value.getX()) {
-                return Math.subtractExact(b, o);
+                return LoopUtility.subtractExact(IntegerStamp.getBits(offset.stamp(NodeView.DEFAULT)), b, o);
             } else {
                 assert base.valueNode() == value.getY() || base instanceof BasicInductionVariable basic && basic.getOp() instanceof IntegerExactArithmeticNode : Assertions.errorMessage(base,
                                 base.valueNode(), value, value.getY());
-                return Math.subtractExact(b, o);
+                return LoopUtility.subtractExact(IntegerStamp.getBits(offset.stamp(NodeView.DEFAULT)), b, o);
             }
         }
         throw GraalError.shouldNotReachHereUnexpectedValue(value); // ExcludeFromJacocoGeneratedReport

compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/nodes/loop/DerivedScaledInductionVariable.java
@@ -34,6 +34,7 @@ import jdk.graal.compiler.nodes.ValueNode;
 import jdk.graal.compiler.nodes.calc.IntegerConvertNode;
 import jdk.graal.compiler.nodes.calc.NegateNode;
 import jdk.graal.compiler.nodes.util.GraphUtil;
+import jdk.graal.compiler.phases.common.util.LoopUtility;
 
 public class DerivedScaledInductionVariable extends DerivedInductionVariable {
 
@@ -121,7 +122,7 @@ public class DerivedScaledInductionVariable extends DerivedInductionVariable {
     }
 
     private long constantInitSafe() throws ArithmeticException {
-        return Math.multiplyExact(base.constantInit(), scale.asJavaConstant().asLong());
+        return opSafe(base.constantInit(), scale.asJavaConstant().asLong());
     }
 
     @Override
@@ -130,7 +131,13 @@ public class DerivedScaledInductionVariable extends DerivedInductionVariable {
     }
 
     private long constantStrideSafe() throws ArithmeticException {
-        return Math.multiplyExact(base.constantStride(), scale.asJavaConstant().asLong());
+        return opSafe(base.constantStride(), scale.asJavaConstant().asLong());
+    }
+
+    private long opSafe(long a, long b) {
+        // we can use scale bits here because all operands (init, scale, stride and extremum) have
+        // by construction equal bit sizes
+        return LoopUtility.multiplyExact(IntegerStamp.getBits(scale.stamp(NodeView.DEFAULT)), a, b);
     }
 
     @Override
@@ -152,7 +159,7 @@ public class DerivedScaledInductionVariable extends DerivedInductionVariable {
     }
 
     private long constantExtremumSafe() throws ArithmeticException {
-        return Math.multiplyExact(base.constantExtremum(), scale.asJavaConstant().asLong());
+        return opSafe(base.constantExtremum(), scale.asJavaConstant().asLong());
     }
 
     @Override

graalvm/graalvm-community-jdk21u

(cherry picked from commit 0ab51074c4a4a7f76d325f82570a001cd4c3e7ed)

compiler/src/jdk.internal.vm.compiler.test/src/org/graalvm/compiler/core/test/LoopFullUnrollTest.java
@@ -24,6 +24,7 @@
  */
 package org.graalvm.compiler.core.test;
 
+import org.graalvm.compiler.api.directives.GraalDirectives;
 import org.graalvm.compiler.debug.DebugContext;
 import org.graalvm.compiler.debug.DebugDumpScope;
 import org.graalvm.compiler.loop.phases.LoopFullUnrollPhase;
@@ -32,6 +33,7 @@ import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.nodes.StructuredGraph.AllowAssumptions;
 import org.graalvm.compiler.nodes.loop.DefaultLoopPolicies;
 import org.graalvm.compiler.nodes.spi.CoreProviders;
+
 import org.junit.Test;
 
 public class LoopFullUnrollTest extends GraalCompilerTest {
@@ -94,5 +96,39 @@ public class LoopFullUnrollTest extends GraalCompilerTest {
         } catch (Throwable e) {
             throw debug.handle(e);
         }
+    }
+
+    public static int snippetFlows() {
+        int init = Integer.MIN_VALUE;
+        int step = -1;
+        int limit = 1;
+        int phi = init;
+        while (Integer.MIN_VALUE - phi < limit) {
+            GraalDirectives.sideEffect();
+            phi = phi + step;
+        }
+        return phi;
+    }
+
+    @Test
+    public void testFlows() {
+        test("snippetFlows");
+    }
+
+    public static int snippetFlows2() {
+        int init = Integer.MAX_VALUE;
+        int step = -8;
+        int limit = 8184;
+        int phi = init;
+        while (Integer.MIN_VALUE - phi < limit) {
+            GraalDirectives.sideEffect();
+            phi = phi + step;
+        }
+        return phi;
+    }
+
+    @Test
+    public void testFlows2() {
+        test("snippetFlows2");
     }
 }

compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/nodes/loop/DerivedOffsetInductionVariable.java
@@ -27,6 +27,7 @@ package org.graalvm.compiler.nodes.loop;
 import static org.graalvm.compiler.nodes.loop.MathUtil.add;
 import static org.graalvm.compiler.nodes.loop.MathUtil.sub;
 
+import org.graalvm.compiler.core.common.type.IntegerStamp;
 import org.graalvm.compiler.core.common.type.Stamp;
 import org.graalvm.compiler.debug.GraalError;
 import org.graalvm.compiler.nodes.NodeView;
@@ -36,6 +37,7 @@ import org.graalvm.compiler.nodes.calc.BinaryArithmeticNode;
 import org.graalvm.compiler.nodes.calc.IntegerConvertNode;
 import org.graalvm.compiler.nodes.calc.NegateNode;
 import org.graalvm.compiler.nodes.calc.SubNode;
+import org.graalvm.compiler.phases.common.util.LoopUtility;
 
 public class DerivedOffsetInductionVariable extends DerivedInductionVariable {
 
@@ -103,7 +105,7 @@ public class DerivedOffsetInductionVariable extends DerivedInductionVariable {
 
     private long constantStrideSafe() throws ArithmeticException {
         if (value instanceof SubNode && base.valueNode() == value.getY()) {
-            return Math.multiplyExact(base.constantStride(), -1);
+            return LoopUtility.multiplyExact(IntegerStamp.getBits(offset.stamp(NodeView.DEFAULT)), base.constantStride(), -1);
         }
         return base.constantStride();
     }
@@ -159,15 +161,17 @@ public class DerivedOffsetInductionVariable extends DerivedInductionVariable {
     }
 
     private long opSafe(long b, long o) throws ArithmeticException {
+        // we can use offset bits in this method because all operands (init, scale, stride and
+        // extremum) have by construction equal bit sizes
         if (value instanceof AddNode) {
-            return Math.addExact(b, o);
+            return LoopUtility.addExact(IntegerStamp.getBits(offset.stamp(NodeView.DEFAULT)), b, o);
         }
         if (value instanceof SubNode) {
             if (base.valueNode() == value.getX()) {
-                return Math.subtractExact(b, o);
+                return LoopUtility.subtractExact(IntegerStamp.getBits(offset.stamp(NodeView.DEFAULT)), b, o);
             } else {
                 assert base.valueNode() == value.getY() : String.format("[base]=%s;[value]=%s", base.valueNode(), value.getY());
-                return Math.subtractExact(b, o);
+                return LoopUtility.subtractExact(IntegerStamp.getBits(offset.stamp(NodeView.DEFAULT)), b, o);
             }
         }
         throw GraalError.shouldNotReachHereUnexpectedValue(value); // ExcludeFromJacocoGeneratedReport

compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/nodes/loop/DerivedScaledInductionVariable.java
@@ -34,6 +34,7 @@ import org.graalvm.compiler.nodes.ValueNode;
 import org.graalvm.compiler.nodes.calc.IntegerConvertNode;
 import org.graalvm.compiler.nodes.calc.NegateNode;
 import org.graalvm.compiler.nodes.util.GraphUtil;
+import org.graalvm.compiler.phases.common.util.LoopUtility;
 
 public class DerivedScaledInductionVariable extends DerivedInductionVariable {
 
@@ -121,7 +122,7 @@ public class DerivedScaledInductionVariable extends DerivedInductionVariable {
     }
 
     private long constantInitSafe() throws ArithmeticException {
-        return Math.multiplyExact(base.constantInit(), scale.asJavaConstant().asLong());
+        return opSafe(base.constantInit(), scale.asJavaConstant().asLong());
     }
 
     @Override
@@ -130,7 +131,13 @@ public class DerivedScaledInductionVariable extends DerivedInductionVariable {
     }
 
     private long constantStrideSafe() throws ArithmeticException {
-        return Math.multiplyExact(base.constantStride(), scale.asJavaConstant().asLong());
+        return opSafe(base.constantStride(), scale.asJavaConstant().asLong());
+    }
+
+    private long opSafe(long a, long b) {
+        // we can use scale bits here because all operands (init, scale, stride and extremum) have
+        // by construction equal bit sizes
+        return LoopUtility.multiplyExact(IntegerStamp.getBits(scale.stamp(NodeView.DEFAULT)), a, b);
     }
 
     @Override
@@ -152,7 +159,7 @@ public class DerivedScaledInductionVariable extends DerivedInductionVariable {
     }
 
     private long constantExtremumSafe() throws ArithmeticException {
-        return Math.multiplyExact(base.constantExtremum(), scale.asJavaConstant().asLong());
+        return opSafe(base.constantExtremum(), scale.asJavaConstant().asLong());
     }
 
     @Override