oracle/graal

In heap dumps, use the slash format instead of the dot format for class names.

substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/os/BufferedFileOperationSupport.java
@@ -43,6 +43,7 @@ import com.oracle.svm.core.feature.InternalFeature;
 import com.oracle.svm.core.hub.DynamicHub;
 import com.oracle.svm.core.hub.LayoutEncoding;
 import com.oracle.svm.core.jdk.UninterruptibleUtils;
+import com.oracle.svm.core.jdk.UninterruptibleUtils.CharReplacer;
 import com.oracle.svm.core.memory.NullableNativeMemory;
 import com.oracle.svm.core.nmt.NmtCategory;
 import com.oracle.svm.core.os.BufferedFileOperationSupport.BufferedFileOperationSupportHolder;
@@ -343,6 +344,11 @@ public class BufferedFileOperationSupport {
         return writeLong(f, Double.doubleToLongBits(v));
     }
 
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
+    public boolean writeUTF8(BufferedFile f, String string) {
+        return writeUTF8(f, string, null);
+    }
+
     /**
      * Writes the String characters encoded as UTF8 to the current file position and advances the
      * file position.
@@ -350,10 +356,14 @@ public class BufferedFileOperationSupport {
      * @return true if the data was written, false otherwise.
      */
     @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
-    public boolean writeUTF8(BufferedFile f, String string) {
+    public boolean writeUTF8(BufferedFile f, String string, CharReplacer replacer) {
         boolean success = true;
         for (int index = 0; index < string.length() && success; index++) {
-            success &= writeUTF8(f, UninterruptibleUtils.String.charAt(string, index));
+            char ch = UninterruptibleUtils.String.charAt(string, index);
+            if (replacer != null) {
+                ch = replacer.replace(ch);
+            }
+            success &= writeUTF8(f, ch);
         }
         return success;
     }

graalvm/graalvm-community-jdk21u

(cherry picked from commit e367a6e0070875650812ae834ab49f1205f71db4)

substratevm/src/com.oracle.svm.core/src/com/oracle/svm/core/os/BufferedFileOperationSupport.java
@@ -47,6 +47,7 @@ import com.oracle.svm.core.feature.InternalFeature;
 import com.oracle.svm.core.hub.DynamicHub;
 import com.oracle.svm.core.hub.LayoutEncoding;
 import com.oracle.svm.core.jdk.JavaLangSubstitutions;
+import com.oracle.svm.core.jdk.UninterruptibleUtils.CharReplacer;
 import com.oracle.svm.core.os.BufferedFileOperationSupport.BufferedFileOperationSupportHolder;
 import com.oracle.svm.core.os.RawFileOperationSupport.RawFileDescriptor;
 import com.oracle.svm.core.snippets.KnownIntrinsics;
@@ -342,6 +343,11 @@ public class BufferedFileOperationSupport {
         return writeLong(f, Double.doubleToLongBits(v));
     }
 
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
+    public boolean writeUTF8(BufferedFile f, String string) {
+        return writeUTF8(f, string, null);
+    }
+
     /**
      * Writes the String characters encoded as UTF8 to the current file position and advances the
      * file position.
@@ -349,10 +355,14 @@ public class BufferedFileOperationSupport {
      * @return true if the data was written, false otherwise.
      */
     @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
-    public boolean writeUTF8(BufferedFile f, String string) {
+    public boolean writeUTF8(BufferedFile f, String string, CharReplacer replacer) {
         boolean success = true;
         for (int index = 0; index < string.length() && success; index++) {
-            success &= writeUTF8(f, JavaLangSubstitutions.StringUtil.charAt(string, index));
+            char ch = JavaLangSubstitutions.StringUtil.charAt(string, index);
+            if (replacer != null) {
+                ch = replacer.replace(ch);
+            }
+            success &= writeUTF8(f, ch);
         }
         return success;
     }