oracle/graal

Infinite recursion in AArch64MacroAssembler.add/sub with immediate = Integer.MIN_VALUE

compiler/src/jdk.graal.compiler.test/src/jdk/graal/compiler/core/aarch64/test/AddSubInfiniteRecursionTest.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package jdk.graal.compiler.core.aarch64.test;
+
+import jdk.graal.compiler.core.test.GraalCompilerTest;
+import org.junit.Test;
+
+/**
+ * Add/sub with an immediate MIN_VALUE may result in infinite recursion since MIN_VALUE < 0 and
+ * -MIN_VALUE < 0.
+ */
+public class AddSubInfiniteRecursionTest extends GraalCompilerTest {
+    public static int testAddIntMinValue(int arg) {
+        return arg + Integer.MIN_VALUE;
+    }
+
+    public static int testSubIntMinValue(int arg) {
+        return arg - Integer.MIN_VALUE;
+    }
+
+    @Test
+    public void runIntMinValue() {
+        test("testAddIntMinValue", 0);
+        test("testSubIntMinValue", 0);
+    }
+}

graalvm/graalvm-community-jdk21u

(cherry picked from commit d48052359e945093aba5c2ebc6455be0ab31c249)

compiler/src/jdk.internal.vm.compiler.test/src/org/graalvm/compiler/core/aarch64/test/AddSubInfiniteRecursionTest.java
@@ -1,0 +1,48 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.graalvm.compiler.core.aarch64.test;
+
+import org.graalvm.compiler.core.test.GraalCompilerTest;
+import org.junit.Test;
+
+/**
+ * Add/sub with an immediate MIN_VALUE may result in infinite recursion since MIN_VALUE < 0 and
+ * -MIN_VALUE < 0.
+ */
+public class AddSubInfiniteRecursionTest extends GraalCompilerTest {
+    public static int testAddIntMinValue(int arg) {
+        return arg + Integer.MIN_VALUE;
+    }
+
+    public static int testSubIntMinValue(int arg) {
+        return arg - Integer.MIN_VALUE;
+    }
+
+    @Test
+    public void runIntMinValue() {
+        test("testAddIntMinValue", 0);
+        test("testSubIntMinValue", 0);
+    }
+}