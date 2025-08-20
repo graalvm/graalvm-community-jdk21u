731eaee1a784155e53f7b19be1a9d7d47bcad633
Boris Ulasevich <Boris.Ulasevich@bell-sw.com>
2025-08-20 12:45:50 -0300

    Update graal-js and graal-nodejs to GraalVM 23.1.8 (JDK 21.0.8)

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -33,7 +33,7 @@ suite = {
                 "name": "graal-nodejs",
                 "subdir": True,
                 "dynamic": True,
-                "version": "11b33a178a9d9838783a5e03f6f3b3d9fea2b489",
+                "version": "5f294912081e699bc204b06a248e9d8b4a9c80ff",
                 "urls" : [
                     {"url" : "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
@@ -42,7 +42,7 @@ suite = {
                 "name": "graal-js",
                 "subdir": True,
                 "dynamic": True,
-                "version": "11b33a178a9d9838783a5e03f6f3b3d9fea2b489",
+                "version": "5f294912081e699bc204b06a248e9d8b4a9c80ff",
                 "urls": [
                     {"url": "https://github.com/graalvm/graaljs.git", "kind" : "git"},
                 ]
