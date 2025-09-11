oracle/graal

Support cross-isolate exception dispatch for isolated compilation.

substratevm/src/com.oracle.svm.graal/src/com/oracle/svm/graal/isolated/IsolatedCompileClient.java
@@ -24,15 +24,20 @@
  */
 package com.oracle.svm.graal.isolated;
 
+import org.graalvm.nativeimage.IsolateThread;
 import org.graalvm.nativeimage.ObjectHandle;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
 import org.graalvm.nativeimage.c.type.CCharPointer;
 import org.graalvm.nativeimage.c.type.CTypeConversion;
+import org.graalvm.word.WordBase;
 
 import com.oracle.svm.core.Uninterruptible;
+import com.oracle.svm.core.c.function.CEntryPointOptions;
 import com.oracle.svm.core.handles.ThreadLocalHandles;
 import com.oracle.svm.core.threadlocal.FastThreadLocalFactory;
 import com.oracle.svm.core.threadlocal.FastThreadLocalObject;
+
+import jdk.graal.compiler.word.Word;
 
 /**
  * Thread-local context object in a client isolate thread, that is, the isolate that has initiated a
@@ -40,7 +45,42 @@ import com.oracle.svm.core.threadlocal.FastThreadLocalObject;
  *
  * @see IsolatedCompileContext
  */
-public final class IsolatedCompileClient {
+public final class IsolatedCompileClient extends IsolatedCompilationExceptionDispatch {
+
+    public static final class ExceptionRethrowCallerEpilogue implements CEntryPointOptions.CallerEpilogue {
+        static void callerEpilogue() {
+            IsolatedCompileClient.throwPendingException();
+        }
+    }
+
+    public static final class VoidExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static void handle(Throwable t) {
+            get().handleException(t);
+        }
+    }
+
+    public static final class IntExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static int handle(Throwable t) {
+            return get().handleException(t);
+        }
+    }
+
+    public static final class BooleanExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static boolean handle(Throwable t) {
+            return get().handleException(t) != 0;
+        }
+    }
+
+    public static final class WordExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static WordBase handle(Throwable t) {
+            int v = get().handleException(t);
+            return Word.signed(v);
+        }
+    }
 
     private static final FastThreadLocalObject<IsolatedCompileClient> currentClient = //
                     FastThreadLocalFactory.createObject(IsolatedCompileClient.class, "IsolatedCompileClient.currentClient");
@@ -63,6 +103,12 @@ public final class IsolatedCompileClient {
     }
 
     public CompilerIsolateThread getCompiler() {
+        return compiler;
+    }
+
+    @Override
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
+    protected IsolateThread getOtherIsolate() {
         return compiler;
     }
 

substratevm/src/com.oracle.svm.graal/src/com/oracle/svm/graal/isolated/IsolatedCompileContext.java
@@ -24,16 +24,25 @@
  */
 package com.oracle.svm.graal.isolated;
 
+import org.graalvm.nativeimage.IsolateThread;
 import org.graalvm.nativeimage.ObjectHandle;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
 import org.graalvm.nativeimage.c.type.CCharPointer;
 import org.graalvm.nativeimage.c.type.CCharPointerPointer;
 import org.graalvm.nativeimage.c.type.CTypeConversion;
+import org.graalvm.word.WordBase;
 
 import com.oracle.svm.core.Uninterruptible;
+import com.oracle.svm.core.c.CGlobalData;
+import com.oracle.svm.core.c.CGlobalDataFactory;
+import com.oracle.svm.core.c.function.CEntryPointActions;
+import com.oracle.svm.core.c.function.CEntryPointErrors;
+import com.oracle.svm.core.c.function.CEntryPointOptions;
 import com.oracle.svm.core.handles.ThreadLocalHandles;
 import com.oracle.svm.core.threadlocal.FastThreadLocalFactory;
 import com.oracle.svm.core.threadlocal.FastThreadLocalObject;
+
+import jdk.graal.compiler.word.Word;
 
 /**
  * Thread-local context object in the thread in the compiler isolate that is compiling on behalf of
@@ -41,14 +50,60 @@ import com.oracle.svm.core.threadlocal.FastThreadLocalObject;
  *
  * @see IsolatedCompileClient
  */
-public final class IsolatedCompileContext {
+public final class IsolatedCompileContext extends IsolatedCompilationExceptionDispatch {
+
+    public static final class ExceptionRethrowCallerEpilogue implements CEntryPointOptions.CallerEpilogue {
+        static void callerEpilogue() {
+            IsolatedCompileContext.throwPendingException();
+        }
+    }
+
+    public static final class VoidExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static void handle(Throwable t) {
+            get().handleException(t);
+        }
+    }
+
+    public static final class WordExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static WordBase handle(Throwable t) {
+            return Word.signed(get().handleException(t));
+        }
+    }
+
+    public static final class ResetContextWordExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static WordBase handle(Throwable t) {
+            int v = get().handleException(t);
+            return Word.signed(v);
+        }
+    }
+
+    public static final class ExitCompilationEpilogue implements CEntryPointOptions.Epilogue {
+        private static final CGlobalData<CCharPointer> errorMessage = CGlobalDataFactory.createCString(
+                        "Failed to leave the current isolated compilation IsolateThread context.");
+
+        @Uninterruptible(reason = "Epilogue")
+        static void epilogue() {
+            set(null);
+
+            int code = CEntryPointActions.leave();
+            if (code != CEntryPointErrors.NO_ERROR) {
+                CEntryPointActions.failFatally(code, errorMessage.get());
+            }
+        }
+    }
+
     private static final FastThreadLocalObject<IsolatedCompileContext> currentContext = //
                     FastThreadLocalFactory.createObject(IsolatedCompileContext.class, "IsolatedCompileContext.currentContext");
 
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
     public static IsolatedCompileContext get() {
         return currentContext.get();
     }
 
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
     public static void set(IsolatedCompileContext context) {
         assert (context == null) != (currentContext.get() == null);
         currentContext.set(context);
@@ -62,6 +117,12 @@ public final class IsolatedCompileContext {
     }
 
     public ClientIsolateThread getClient() {
+        return client;
+    }
+
+    @Override
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
+    protected IsolateThread getOtherIsolate() {
         return client;
     }
 

substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/code/CEntryPointJavaCallStubMethod.java
@@ -30,10 +30,15 @@ import java.util.List;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
 import org.graalvm.nativeimage.c.function.CFunctionPointer;
 
+import com.oracle.graal.pointsto.meta.AnalysisMethod;
+import com.oracle.graal.pointsto.meta.AnalysisType;
 import com.oracle.svm.core.c.BoxedRelocatedPointer;
+import com.oracle.svm.core.c.function.CEntryPointOptions;
 import com.oracle.svm.core.thread.VMThreads.StatusSupport;
+import com.oracle.svm.core.util.UserError;
 import com.oracle.svm.hosted.phases.HostedGraphKit;
 
+import jdk.graal.compiler.nodes.CallTargetNode;
 import jdk.graal.compiler.nodes.ConstantNode;
 import jdk.graal.compiler.nodes.ValueNode;
 import jdk.graal.compiler.nodes.java.LoadFieldNode;
@@ -70,6 +75,18 @@ public class CEntryPointJavaCallStubMethod extends CCallStubMethod {
     @Override
     protected String getCorrespondingAnnotationName() {
         return CEntryPoint.class.getSimpleName();
+    }
+
+    @Override
+    protected void emitCallerEpilogue(HostedGraphKit kit) {
+        CEntryPointOptions options = getOriginal().getAnnotation(CEntryPointOptions.class);
+        if (options != null && options.callerEpilogue() != null && options.callerEpilogue() != CEntryPointOptions.NoCallerEpilogue.class) {
+            AnalysisType epilogue = kit.getMetaAccess().lookupJavaType(options.callerEpilogue());
+            AnalysisMethod[] epilogueMethods = epilogue.getDeclaredMethods(false);
+            UserError.guarantee(epilogueMethods.length == 1 && epilogueMethods[0].isStatic() && epilogueMethods[0].getSignature().getParameterCount(false) == 0,
+                            "Caller epilogue class must declare exactly one static method without parameters: %s -> %s", getOriginal(), epilogue);
+            kit.createInvokeWithExceptionAndUnwind(epilogueMethods[0], CallTargetNode.InvokeKind.Static, kit.getFrameState(), kit.bci());
+        }
     }
 
     @Override

graalvm/graalvm-community-jdk21u

(cherry picked from commit c11d974c85bbc02075f67a78d59a1327f21507ea)

substratevm/src/com.oracle.svm.graal/src/com/oracle/svm/graal/isolated/IsolatedCompileClient.java
@@ -24,12 +24,16 @@
  */
 package com.oracle.svm.graal.isolated;
 
+import org.graalvm.nativeimage.IsolateThread;
 import org.graalvm.nativeimage.ObjectHandle;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
 import org.graalvm.nativeimage.c.type.CCharPointer;
 import org.graalvm.nativeimage.c.type.CTypeConversion;
+import org.graalvm.word.WordBase;
+import org.graalvm.word.WordFactory;
 
 import com.oracle.svm.core.Uninterruptible;
+import com.oracle.svm.core.c.function.CEntryPointOptions;
 import com.oracle.svm.core.handles.ThreadLocalHandles;
 import com.oracle.svm.core.threadlocal.FastThreadLocalFactory;
 import com.oracle.svm.core.threadlocal.FastThreadLocalObject;
@@ -40,7 +44,42 @@ import com.oracle.svm.core.threadlocal.FastThreadLocalObject;
  *
  * @see IsolatedCompileContext
  */
-public final class IsolatedCompileClient {
+public final class IsolatedCompileClient extends IsolatedCompilationExceptionDispatch {
+
+    public static final class ExceptionRethrowCallerEpilogue implements CEntryPointOptions.CallerEpilogue {
+        static void callerEpilogue() {
+            IsolatedCompileClient.throwPendingException();
+        }
+    }
+
+    public static final class VoidExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static void handle(Throwable t) {
+            get().handleException(t);
+        }
+    }
+
+    public static final class IntExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static int handle(Throwable t) {
+            return get().handleException(t);
+        }
+    }
+
+    public static final class BooleanExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static boolean handle(Throwable t) {
+            return get().handleException(t) != 0;
+        }
+    }
+
+    public static final class WordExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static WordBase handle(Throwable t) {
+            int v = get().handleException(t);
+            return WordFactory.signed(v);
+        }
+    }
 
     private static final FastThreadLocalObject<IsolatedCompileClient> currentClient = //
                     FastThreadLocalFactory.createObject(IsolatedCompileClient.class, "IsolatedCompileClient.currentClient");
@@ -63,6 +102,12 @@ public final class IsolatedCompileClient {
     }
 
     public CompilerIsolateThread getCompiler() {
+        return compiler;
+    }
+
+    @Override
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
+    protected IsolateThread getOtherIsolate() {
         return compiler;
     }
 

substratevm/src/com.oracle.svm.graal/src/com/oracle/svm/graal/isolated/IsolatedCompileContext.java
@@ -24,13 +24,21 @@
  */
 package com.oracle.svm.graal.isolated;
 
+import org.graalvm.nativeimage.IsolateThread;
 import org.graalvm.nativeimage.ObjectHandle;
 import org.graalvm.nativeimage.c.function.CEntryPoint;
 import org.graalvm.nativeimage.c.type.CCharPointer;
 import org.graalvm.nativeimage.c.type.CCharPointerPointer;
 import org.graalvm.nativeimage.c.type.CTypeConversion;
+import org.graalvm.word.WordBase;
+import org.graalvm.word.WordFactory;
 
 import com.oracle.svm.core.Uninterruptible;
+import com.oracle.svm.core.c.CGlobalData;
+import com.oracle.svm.core.c.CGlobalDataFactory;
+import com.oracle.svm.core.c.function.CEntryPointActions;
+import com.oracle.svm.core.c.function.CEntryPointErrors;
+import com.oracle.svm.core.c.function.CEntryPointOptions;
 import com.oracle.svm.core.handles.ThreadLocalHandles;
 import com.oracle.svm.core.threadlocal.FastThreadLocalFactory;
 import com.oracle.svm.core.threadlocal.FastThreadLocalObject;
@@ -41,14 +49,60 @@ import com.oracle.svm.core.threadlocal.FastThreadLocalObject;
  *
  * @see IsolatedCompileClient
  */
-public final class IsolatedCompileContext {
+public final class IsolatedCompileContext extends IsolatedCompilationExceptionDispatch {
+
+    public static final class ExceptionRethrowCallerEpilogue implements CEntryPointOptions.CallerEpilogue {
+        static void callerEpilogue() {
+            IsolatedCompileContext.throwPendingException();
+        }
+    }
+
+    public static final class VoidExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static void handle(Throwable t) {
+            get().handleException(t);
+        }
+    }
+
+    public static final class WordExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static WordBase handle(Throwable t) {
+            return WordFactory.signed(get().handleException(t));
+        }
+    }
+
+    public static final class ResetContextWordExceptionHandler implements CEntryPoint.ExceptionHandler {
+        @Uninterruptible(reason = "Exception handler")
+        static WordBase handle(Throwable t) {
+            int v = get().handleException(t);
+            return WordFactory.signed(v);
+        }
+    }
+
+    public static final class ExitCompilationEpilogue implements CEntryPointOptions.Epilogue {
+        private static final CGlobalData<CCharPointer> errorMessage = CGlobalDataFactory.createCString(
+                        "Failed to leave the current isolated compilation IsolateThread context.");
+
+        @Uninterruptible(reason = "Epilogue")
+        static void epilogue() {
+            set(null);
+
+            int code = CEntryPointActions.leave();
+            if (code != CEntryPointErrors.NO_ERROR) {
+                CEntryPointActions.failFatally(code, errorMessage.get());
+            }
+        }
+    }
+
     private static final FastThreadLocalObject<IsolatedCompileContext> currentContext = //
                     FastThreadLocalFactory.createObject(IsolatedCompileContext.class, "IsolatedCompileContext.currentContext");
 
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
     public static IsolatedCompileContext get() {
         return currentContext.get();
     }
 
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
     public static void set(IsolatedCompileContext context) {
         assert (context == null) != (currentContext.get() == null);
         currentContext.set(context);
@@ -62,6 +116,12 @@ public final class IsolatedCompileContext {
     }
 
     public ClientIsolateThread getClient() {
+        return client;
+    }
+
+    @Override
+    @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
+    protected IsolateThread getOtherIsolate() {
         return client;
     }
 

substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/code/CEntryPointJavaCallStubMethod.java
@@ -27,6 +27,7 @@ package com.oracle.svm.hosted.code;
 import java.lang.reflect.AnnotatedElement;
 import java.util.List;
 
+import org.graalvm.compiler.nodes.CallTargetNode;
 import org.graalvm.compiler.nodes.ConstantNode;
 import org.graalvm.compiler.nodes.ValueNode;
 import org.graalvm.compiler.nodes.java.LoadFieldNode;
@@ -35,7 +36,9 @@ import org.graalvm.nativeimage.c.function.CFunctionPointer;
 
 import com.oracle.graal.pointsto.meta.HostedProviders;
 import com.oracle.svm.core.c.BoxedRelocatedPointer;
+import com.oracle.svm.core.c.function.CEntryPointOptions;
 import com.oracle.svm.core.thread.VMThreads.StatusSupport;
+import com.oracle.svm.core.util.UserError;
 import com.oracle.svm.hosted.phases.HostedGraphKit;
 
 import jdk.vm.ci.meta.ResolvedJavaMethod;
@@ -71,6 +74,18 @@ public class CEntryPointJavaCallStubMethod extends CCallStubMethod {
     @Override
     protected String getCorrespondingAnnotationName() {
         return CEntryPoint.class.getSimpleName();
+    }
+
+    @Override
+    protected void emitCallerEpilogue(HostedGraphKit kit) {
+        CEntryPointOptions options = getOriginal().getAnnotation(CEntryPointOptions.class);
+        if (options != null && options.callerEpilogue() != null && options.callerEpilogue() != CEntryPointOptions.NoCallerEpilogue.class) {
+            ResolvedJavaType epilogue = kit.getMetaAccess().lookupJavaType(options.callerEpilogue());
+            ResolvedJavaMethod[] epilogueMethods = epilogue.getDeclaredMethods(false);
+            UserError.guarantee(epilogueMethods.length == 1 && epilogueMethods[0].isStatic() && epilogueMethods[0].getSignature().getParameterCount(false) == 0,
+                            "Caller epilogue class must declare exactly one static method without parameters: %s -> %s", getOriginal(), epilogue);
+            kit.createInvokeWithExceptionAndUnwind(epilogueMethods[0], CallTargetNode.InvokeKind.Static, kit.getFrameState(), kit.bci());
+        }
     }
 
     @Override