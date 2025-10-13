b1de94feb3959eaa106cb7a3af0ebe42d87f1d49
Foivos Zakkak <fzakkak@redhat.com>
2025-10-13 16:01:18 +0300

    Mark suite files for upcoming 23.1.10 release

--- vm/mx.vm/suite.py
+++ vm/mx.vm/suite.py
@@ -1,8 +1,8 @@
 suite = {
     "name": "vm",
-    "version" : "23.1.9",
+    "version" : "23.1.10",
     "mxversion": "6.41.0",
-    "release" : True,
+    "release" : False,
     "groupId" : "org.graalvm",
 
     "url" : "http://www.graalvm.org/",
