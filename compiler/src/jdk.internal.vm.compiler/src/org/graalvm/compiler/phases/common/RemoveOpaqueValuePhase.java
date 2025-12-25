oracle/graal

[GR-52596] Restrict applicability of RemoveOpaqueValuePhase.

compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/phases/common/RemoveOpaqueValuePhase.java
@@ -28,6 +28,7 @@ import java.util.Optional;
 
 import jdk.graal.compiler.nodes.GraphState;
 import jdk.graal.compiler.nodes.StructuredGraph;
+import jdk.graal.compiler.nodes.GraphState.StageFlag;
 import jdk.graal.compiler.nodes.extended.OpaqueValueNode;
 import jdk.graal.compiler.nodes.spi.CoreProviders;
 import jdk.graal.compiler.phases.BasePhase;
@@ -38,7 +39,9 @@ import jdk.graal.compiler.phases.BasePhase;
 public class RemoveOpaqueValuePhase extends BasePhase<CoreProviders> {
     @Override
     public Optional<NotApplicable> notApplicableTo(GraphState graphState) {
-        return ALWAYS_APPLICABLE;
+        return NotApplicable.ifAny(
+                        NotApplicable.unlessRunAfter(this, StageFlag.LOW_TIER_LOWERING, graphState),
+                        NotApplicable.ifApplied(this, StageFlag.REMOVE_OPAQUE_VALUES, graphState));
     }
 
     @Override
@@ -51,5 +54,11 @@ public class RemoveOpaqueValuePhase extends BasePhase<CoreProviders> {
         for (OpaqueValueNode opaque : graph.getNodes(OpaqueValueNode.TYPE)) {
             opaque.replaceAtUsagesAndDelete(opaque.getValue());
         }
+    }
+
+    @Override
+    public void updateGraphState(GraphState graphState) {
+        super.updateGraphState(graphState);
+        graphState.setAfterStage(StageFlag.REMOVE_OPAQUE_VALUES);
     }
 }

graalvm/graalvm-community-jdk21u

(cherry picked from commit ac2a5f85317e2b23c572c3970d49baed861e6853)

compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/phases/common/RemoveOpaqueValuePhase.java
@@ -27,6 +27,7 @@ package org.graalvm.compiler.phases.common;
 import java.util.Optional;
 
 import org.graalvm.compiler.nodes.GraphState;
+import org.graalvm.compiler.nodes.GraphState.StageFlag;
 import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.nodes.extended.OpaqueValueNode;
 import org.graalvm.compiler.nodes.spi.CoreProviders;
@@ -38,7 +39,9 @@ import org.graalvm.compiler.phases.BasePhase;
 public class RemoveOpaqueValuePhase extends BasePhase<CoreProviders> {
     @Override
     public Optional<NotApplicable> notApplicableTo(GraphState graphState) {
-        return ALWAYS_APPLICABLE;
+        return NotApplicable.ifAny(
+                        NotApplicable.unlessRunAfter(this, StageFlag.LOW_TIER_LOWERING, graphState),
+                        NotApplicable.ifApplied(this, StageFlag.REMOVE_OPAQUE_VALUES, graphState));
     }
 
     public boolean shouldApply(StructuredGraph graph) {
@@ -50,5 +53,11 @@ public class RemoveOpaqueValuePhase extends BasePhase<CoreProviders> {
         for (OpaqueValueNode opaque : graph.getNodes(OpaqueValueNode.TYPE)) {
             opaque.replaceAtUsagesAndDelete(opaque.getValue());
         }
+    }
+
+    @Override
+    public void updateGraphState(GraphState graphState) {
+        super.updateGraphState(graphState);
+        graphState.setAfterStage(StageFlag.REMOVE_OPAQUE_VALUES);
     }
 }