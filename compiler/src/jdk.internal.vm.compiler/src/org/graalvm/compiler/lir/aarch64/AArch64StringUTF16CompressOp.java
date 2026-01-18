oracle/graal

Replace custom AArch64StringUTF16CompressOp with AArch64EncodeArrayOp.

compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/lir/aarch64/AArch64StringUTF16CompressOp.java
@@ -25,206 +25,70 @@
  */
 package jdk.graal.compiler.lir.aarch64;
 
-import static jdk.vm.ci.aarch64.AArch64.SIMD;
+import static jdk.graal.compiler.lir.LIRInstruction.OperandFlag.REG;
 import static jdk.vm.ci.aarch64.AArch64.zr;
 import static jdk.vm.ci.code.ValueUtil.asRegister;
-import static jdk.graal.compiler.lir.LIRInstruction.OperandFlag.REG;
 
-import jdk.graal.compiler.asm.Label;
-import jdk.graal.compiler.asm.aarch64.AArch64ASIMDAssembler;
-import jdk.graal.compiler.asm.aarch64.AArch64ASIMDAssembler.ElementSize;
-import jdk.graal.compiler.asm.aarch64.AArch64Address;
-import jdk.graal.compiler.asm.aarch64.AArch64Address.AddressingMode;
 import jdk.graal.compiler.asm.aarch64.AArch64Assembler;
-import jdk.graal.compiler.asm.aarch64.AArch64Assembler.ConditionFlag;
 import jdk.graal.compiler.asm.aarch64.AArch64MacroAssembler;
-import jdk.graal.compiler.asm.aarch64.AArch64MacroAssembler.ScratchRegister;
-import jdk.graal.compiler.core.common.LIRKind;
-import jdk.graal.compiler.lir.asm.CompilationResultBuilder;
+import jdk.graal.compiler.debug.GraalError;
 import jdk.graal.compiler.lir.LIRInstructionClass;
 import jdk.graal.compiler.lir.Opcode;
+import jdk.graal.compiler.lir.SyncPort;
+import jdk.graal.compiler.lir.asm.CompilationResultBuilder;
 import jdk.graal.compiler.lir.gen.LIRGeneratorTool;
-
+import jdk.graal.compiler.serviceprovider.JavaVersionUtil;
 import jdk.vm.ci.aarch64.AArch64Kind;
 import jdk.vm.ci.code.Register;
 import jdk.vm.ci.meta.AllocatableValue;
+import jdk.vm.ci.meta.Value;
 
+// @formatter:off
+@SyncPort(from = "https://github.com/openjdk/jdk/blob/c51685267c7bd5a7cee27ebc2bf0d9899cda9d4c/src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp#L5887-L5897",
+          sha1 = "857dc6f9a492da6c8e20afb2139ae393efd228ac")
+// @formatter:on
 @Opcode("AArch64_STRING_COMPRESS")
-public final class AArch64StringUTF16CompressOp extends AArch64LIRInstruction {
+public final class AArch64StringUTF16CompressOp extends AArch64ComplexVectorOp {
     public static final LIRInstructionClass<AArch64StringUTF16CompressOp> TYPE = LIRInstructionClass.create(AArch64StringUTF16CompressOp.class);
 
-    private static final int CHUNK_ELEMENT_COUNT = 16;
-
     @Def({REG}) protected AllocatableValue resultValue;
-    @Use({REG}) protected AllocatableValue len;
-    @Alive({REG}) protected AllocatableValue src;
-    @Alive({REG}) protected AllocatableValue dst;
-    @Temp({REG}) protected AllocatableValue temp1;
-    @Temp({REG}) protected AllocatableValue temp2;
-    @Temp({REG}) protected AllocatableValue temp3;
-    @Temp({REG}) protected AllocatableValue vectorTemp1;
-    @Temp({REG}) protected AllocatableValue vectorTemp2;
-    @Temp({REG}) protected AllocatableValue vectorTemp3;
+    @Alive({REG}) protected AllocatableValue lenValue;
+    @Alive({REG}) protected AllocatableValue srcValue;
+    @Alive({REG}) protected AllocatableValue dstValue;
+    @Temp({REG}) protected AllocatableValue[] temp;
+    @Temp({REG}) protected Value[] vectorTemp;
 
     public AArch64StringUTF16CompressOp(LIRGeneratorTool tool, AllocatableValue src, AllocatableValue dst, AllocatableValue len, AllocatableValue result) {
         super(TYPE);
-        assert result.getPlatformKind().equals(AArch64Kind.DWORD) : result;
-        assert len.getPlatformKind().equals(AArch64Kind.DWORD) : len;
-        assert src.getPlatformKind().equals(AArch64Kind.QWORD) : src;
-        assert dst.getPlatformKind().equals(AArch64Kind.QWORD) : dst;
+        GraalError.guarantee(result.getPlatformKind().equals(AArch64Kind.DWORD), "int value expected");
+        GraalError.guarantee(len.getPlatformKind().equals(AArch64Kind.DWORD), "int value expected");
+        GraalError.guarantee(src.getPlatformKind().equals(AArch64Kind.QWORD), "pointer value expected");
+        GraalError.guarantee(dst.getPlatformKind().equals(AArch64Kind.QWORD), "pointer value expected");
 
-        this.len = len;
-        this.src = src;
-        this.dst = dst;
+        this.lenValue = len;
+        this.srcValue = src;
+        this.dstValue = dst;
         resultValue = result;
-        LIRKind archWordKind = LIRKind.value(AArch64Kind.QWORD);
-        temp1 = tool.newVariable(archWordKind);
-        temp2 = tool.newVariable(archWordKind);
-        temp3 = tool.newVariable(archWordKind);
-        LIRKind vectorKind = LIRKind.value(tool.target().arch.getLargestStorableKind(SIMD));
-        vectorTemp1 = tool.newVariable(vectorKind);
-        vectorTemp2 = tool.newVariable(vectorKind);
-        vectorTemp3 = tool.newVariable(vectorKind);
+        temp = allocateTempRegisters(tool, 3);
+        vectorTemp = AArch64EncodeArrayOp.allocateVectorRegisters(tool, LIRGeneratorTool.CharsetName.ISO_8859_1);
     }
 
     @Override
     public void emitCode(CompilationResultBuilder crb, AArch64MacroAssembler masm) {
-        Label simdImpl = new Label();
-        Label scalarImpl = new Label();
-        Label done = new Label();
+        Register src = asRegister(temp[0]);
+        Register dst = asRegister(temp[1]);
+        Register len = asRegister(temp[2]);
+        Register res = asRegister(resultValue);
 
-        /*
-         * Initialise result for successful return (a sign-extended length). Note length is
-         * guaranteed to be a non-negative value, so this is equivalent to zero-extending length.
-         */
-        Register result = asRegister(resultValue);
-        masm.sxt(64, 32, result, asRegister(len));
+        masm.mov(64, src, asRegister(srcValue));
+        masm.mov(64, dst, asRegister(dstValue));
+        masm.mov(32, len, asRegister(lenValue));
 
-        // return immediately if length is zero
-        masm.cbz(32, result, done);
-
-        Register srcAddress = asRegister(temp1);
-        Register destAddress = asRegister(temp2);
-        masm.mov(64, srcAddress, asRegister(src));
-        masm.mov(64, destAddress, asRegister(dst));
-
-        masm.compare(64, result, CHUNK_ELEMENT_COUNT);
-        masm.branchConditionally(ConditionFlag.GE, simdImpl);
-
-        masm.bind(scalarImpl);
-        emitScalar(masm, done, srcAddress, destAddress, result);
-        masm.jmp(done);
-
-        masm.bind(simdImpl);
-        emitSIMD(masm, scalarImpl, done, srcAddress, destAddress, result);
-
-        masm.bind(done);
-    }
-
-    private static void emitScalar(AArch64MacroAssembler masm, Label done, Register srcAddress, Register destAddress, Register result) {
-        Label failToCompress = new Label();
-        Label scalarLoop = new Label();
-
-        try (ScratchRegister scratchReg1 = masm.getScratchRegister(); ScratchRegister scratchReg2 = masm.getScratchRegister()) {
-            Register val = scratchReg1.getRegister();
-            Register count = scratchReg2.getRegister();
-
-            masm.mov(64, count, result);
-
-            masm.align(AArch64MacroAssembler.PREFERRED_LOOP_ALIGNMENT);
-            masm.bind(scalarLoop);
-            masm.ldr(16, val, AArch64Address.createImmediateAddress(16, AddressingMode.IMMEDIATE_POST_INDEXED, srcAddress, 2));
-            /*
-             * If the current char is greater than 0xFF, stop compressing and return zero as result.
-             */
-            masm.compare(64, val, 0xFF);
-            masm.branchConditionally(ConditionFlag.GT, failToCompress);
-
-            masm.str(8, val, AArch64Address.createImmediateAddress(8, AddressingMode.IMMEDIATE_POST_INDEXED, destAddress, 1));
-            masm.subs(64, count, count, 1);
-            masm.branchConditionally(ConditionFlag.GT, scalarLoop);
-            masm.jmp(done);
-
-            masm.bind(failToCompress);
-            masm.mov(64, result, zr);
+        AArch64EncodeArrayOp.emitEncodeArrayOp(masm, res, src, dst, len, vectorTemp, LIRGeneratorTool.CharsetName.ISO_8859_1);
+        if (JavaVersionUtil.JAVA_SPEC < 22) {
+            // legacy behavior: if (result != length) { result = 0; }
+            masm.cmp(32, res, asRegister(lenValue));
+            masm.csel(32, res, res, zr, AArch64Assembler.ConditionFlag.EQ);
         }
-    }
-
-    private void emitSIMD(AArch64MacroAssembler masm, Label scalarImpl, Label done, Register srcChunkAddress, Register destChunkAddress, Register result) {
-        Register chunkPart1RegV = asRegister(vectorTemp1);
-        Register chunkPart2RegV = asRegister(vectorTemp2);
-        Register tmpRegV1 = asRegister(vectorTemp3);
-        Register tmp = asRegister(temp3);
-
-        Label simdLoop = new Label();
-        Label failToCompress = new Label();
-        Label redoEntireChunk = new Label();
-
-        try (ScratchRegister scratchRegister1 = masm.getScratchRegister(); ScratchRegister scratchRegister2 = masm.getScratchRegister()) {
-            Register lastChunkAddress = scratchRegister1.getRegister();
-            Register endOfSrcAddress = scratchRegister2.getRegister();
-            masm.add(64, endOfSrcAddress, srcChunkAddress, result, AArch64Assembler.ShiftType.LSL, 1);
-            masm.sub(64, lastChunkAddress, endOfSrcAddress, CHUNK_ELEMENT_COUNT * 2);
-
-            masm.align(AArch64MacroAssembler.PREFERRED_LOOP_ALIGNMENT);
-            masm.bind(simdLoop);
-            masm.fldp(128, chunkPart1RegV, chunkPart2RegV, AArch64Address.createImmediateAddress(128, AddressingMode.IMMEDIATE_PAIR_POST_INDEXED, srcChunkAddress, CHUNK_ELEMENT_COUNT * 2));
-            /*
-             * If an element in the chunk is greater than 0xFF, stop compressing and retry with
-             * scalar code.
-             *
-             * To do so, we check for any top bytes which are not zero.
-             */
-            masm.neon.orrVVV(AArch64ASIMDAssembler.ASIMDSize.FullReg, tmpRegV1, chunkPart1RegV, chunkPart2RegV);
-            masm.neon.uzp2VVV(AArch64ASIMDAssembler.ASIMDSize.FullReg, ElementSize.Byte, tmpRegV1, tmpRegV1, tmpRegV1);
-            masm.neon.umovGX(ElementSize.DoubleWord, tmp, tmpRegV1, 0);
-            masm.cbnz(64, tmp, failToCompress);
-
-            // compress elements
-            masm.neon.xtnVV(ElementSize.Byte, tmpRegV1, chunkPart1RegV);
-            masm.neon.xtn2VV(ElementSize.Byte, tmpRegV1, chunkPart2RegV);
-            masm.fstr(128, tmpRegV1, AArch64Address.createImmediateAddress(128, AddressingMode.IMMEDIATE_POST_INDEXED, destChunkAddress, CHUNK_ELEMENT_COUNT));
-            masm.cmp(64, srcChunkAddress, lastChunkAddress);
-            masm.branchConditionally(ConditionFlag.LO, simdLoop);
-
-            /*
-             * Process the last chunk. Move the source position back to the last chunk, 16 bytes
-             * before the end of the input array. Move the destination position back twice the
-             * movement of source position.
-             */
-            masm.cmp(64, srcChunkAddress, endOfSrcAddress);
-            masm.branchConditionally(ConditionFlag.HS, done);
-            masm.sub(64, srcChunkAddress, srcChunkAddress, lastChunkAddress);
-            // srcChunkAddress will be a value in the range (1, 31), so LSR and ASR are equivalent
-            masm.sub(64, destChunkAddress, destChunkAddress, srcChunkAddress, AArch64Assembler.ShiftType.LSR, 1);
-            masm.mov(64, srcChunkAddress, lastChunkAddress);
-            masm.jmp(simdLoop);
-        }
-
-        masm.bind(failToCompress);
-        /*
-         * Determine whether the first un-compressible element was in the first chunk. Compress and
-         * copy the first 16 bytes if the first un-compressible chars is in the second chunk.
-         * Afterwards, jump to the scalar loop to process the failed chunk element-by-element.
-         *
-         * Because we start immediately at the failing chunk part, we know at most 8 elements need
-         * to be checked to find the mismatch.
-         */
-        masm.mov(result, 8);
-        masm.neon.uzp2VVV(AArch64ASIMDAssembler.ASIMDSize.FullReg, ElementSize.Byte, tmpRegV1, chunkPart1RegV, chunkPart1RegV);
-        masm.neon.umovGX(ElementSize.DoubleWord, tmp, tmpRegV1, 0);
-        masm.cbnz(64, tmp, redoEntireChunk);
-
-        // fall-through: store chunkPart1 and start search at chunkPart2
-        // note this is really CHUNK_ELEMENT_SIZE * 2 / 2
-        masm.sub(64, srcChunkAddress, srcChunkAddress, CHUNK_ELEMENT_COUNT);
-        masm.neon.xtnVV(ElementSize.Byte, tmpRegV1, chunkPart1RegV);
-        masm.fstr(64, tmpRegV1, AArch64Address.createImmediateAddress(64, AddressingMode.IMMEDIATE_POST_INDEXED, destChunkAddress, CHUNK_ELEMENT_COUNT / 2));
-        masm.jmp(scalarImpl);
-
-        // start at chunkPart1
-        masm.bind(redoEntireChunk);
-        masm.sub(64, srcChunkAddress, srcChunkAddress, CHUNK_ELEMENT_COUNT * 2);
-        masm.jmp(scalarImpl);
     }
 }

graalvm/graalvm-community-jdk21u

(cherry picked from commit af703de350d9a3efc98ab6e2719f286b5795a35c)

compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/lir/aarch64/AArch64StringUTF16CompressOp.java
@@ -25,206 +25,71 @@
  */
 package org.graalvm.compiler.lir.aarch64;
 
-import static jdk.vm.ci.aarch64.AArch64.SIMD;
 import static jdk.vm.ci.aarch64.AArch64.zr;
 import static jdk.vm.ci.code.ValueUtil.asRegister;
 import static org.graalvm.compiler.lir.LIRInstruction.OperandFlag.REG;
 
-import org.graalvm.compiler.asm.Label;
-import org.graalvm.compiler.asm.aarch64.AArch64ASIMDAssembler;
-import org.graalvm.compiler.asm.aarch64.AArch64ASIMDAssembler.ElementSize;
-import org.graalvm.compiler.asm.aarch64.AArch64Address;
-import org.graalvm.compiler.asm.aarch64.AArch64Address.AddressingMode;
 import org.graalvm.compiler.asm.aarch64.AArch64Assembler;
-import org.graalvm.compiler.asm.aarch64.AArch64Assembler.ConditionFlag;
 import org.graalvm.compiler.asm.aarch64.AArch64MacroAssembler;
-import org.graalvm.compiler.asm.aarch64.AArch64MacroAssembler.ScratchRegister;
-import org.graalvm.compiler.core.common.LIRKind;
+import org.graalvm.compiler.debug.GraalError;
 import org.graalvm.compiler.lir.LIRInstructionClass;
 import org.graalvm.compiler.lir.Opcode;
 import org.graalvm.compiler.lir.asm.CompilationResultBuilder;
 import org.graalvm.compiler.lir.gen.LIRGeneratorTool;
+import org.graalvm.compiler.lir.SyncPort;
+import org.graalvm.compiler.serviceprovider.JavaVersionUtil;
 
 import jdk.vm.ci.aarch64.AArch64Kind;
 import jdk.vm.ci.code.Register;
 import jdk.vm.ci.meta.AllocatableValue;
+import jdk.vm.ci.meta.Value;
 
+// @formatter:off
+@SyncPort(from = "https://github.com/openjdk/jdk/blob/c51685267c7bd5a7cee27ebc2bf0d9899cda9d4c/src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp#L5887-L5897",
+          sha1 = "857dc6f9a492da6c8e20afb2139ae393efd228ac")
+// @formatter:on
 @Opcode("AArch64_STRING_COMPRESS")
-public final class AArch64StringUTF16CompressOp extends AArch64LIRInstruction {
+public final class AArch64StringUTF16CompressOp extends AArch64ComplexVectorOp {
     public static final LIRInstructionClass<AArch64StringUTF16CompressOp> TYPE = LIRInstructionClass.create(AArch64StringUTF16CompressOp.class);
 
-    private static final int CHUNK_ELEMENT_COUNT = 16;
-
     @Def({REG}) protected AllocatableValue resultValue;
-    @Use({REG}) protected AllocatableValue len;
-    @Alive({REG}) protected AllocatableValue src;
-    @Alive({REG}) protected AllocatableValue dst;
-    @Temp({REG}) protected AllocatableValue temp1;
-    @Temp({REG}) protected AllocatableValue temp2;
-    @Temp({REG}) protected AllocatableValue temp3;
-    @Temp({REG}) protected AllocatableValue vectorTemp1;
-    @Temp({REG}) protected AllocatableValue vectorTemp2;
-    @Temp({REG}) protected AllocatableValue vectorTemp3;
+    @Alive({REG}) protected AllocatableValue lenValue;
+    @Alive({REG}) protected AllocatableValue srcValue;
+    @Alive({REG}) protected AllocatableValue dstValue;
+    @Temp({REG}) protected AllocatableValue[] temp;
+    @Temp({REG}) protected Value[] vectorTemp;
 
     public AArch64StringUTF16CompressOp(LIRGeneratorTool tool, AllocatableValue src, AllocatableValue dst, AllocatableValue len, AllocatableValue result) {
         super(TYPE);
-        assert result.getPlatformKind().equals(AArch64Kind.DWORD) : result;
-        assert len.getPlatformKind().equals(AArch64Kind.DWORD) : len;
-        assert src.getPlatformKind().equals(AArch64Kind.QWORD) : src;
-        assert dst.getPlatformKind().equals(AArch64Kind.QWORD) : dst;
+        GraalError.guarantee(result.getPlatformKind().equals(AArch64Kind.DWORD), "int value expected");
+        GraalError.guarantee(len.getPlatformKind().equals(AArch64Kind.DWORD), "int value expected");
+        GraalError.guarantee(src.getPlatformKind().equals(AArch64Kind.QWORD), "pointer value expected");
+        GraalError.guarantee(dst.getPlatformKind().equals(AArch64Kind.QWORD), "pointer value expected");
 
-        this.len = len;
-        this.src = src;
-        this.dst = dst;
+        this.lenValue = len;
+        this.srcValue = src;
+        this.dstValue = dst;
         resultValue = result;
-        LIRKind archWordKind = LIRKind.value(AArch64Kind.QWORD);
-        temp1 = tool.newVariable(archWordKind);
-        temp2 = tool.newVariable(archWordKind);
-        temp3 = tool.newVariable(archWordKind);
-        LIRKind vectorKind = LIRKind.value(tool.target().arch.getLargestStorableKind(SIMD));
-        vectorTemp1 = tool.newVariable(vectorKind);
-        vectorTemp2 = tool.newVariable(vectorKind);
-        vectorTemp3 = tool.newVariable(vectorKind);
+        temp = allocateTempRegisters(tool, 3);
+        vectorTemp = AArch64EncodeArrayOp.allocateVectorRegisters(tool, LIRGeneratorTool.CharsetName.ISO_8859_1);
     }
 
     @Override
     public void emitCode(CompilationResultBuilder crb, AArch64MacroAssembler masm) {
-        Label simdImpl = new Label();
-        Label scalarImpl = new Label();
-        Label done = new Label();
+        Register src = asRegister(temp[0]);
+        Register dst = asRegister(temp[1]);
+        Register len = asRegister(temp[2]);
+        Register res = asRegister(resultValue);
 
-        /*
-         * Initialise result for successful return (a sign-extended length). Note length is
-         * guaranteed to be a non-negative value, so this is equivalent to zero-extending length.
-         */
-        Register result = asRegister(resultValue);
-        masm.sxt(64, 32, result, asRegister(len));
+        masm.mov(64, src, asRegister(srcValue));
+        masm.mov(64, dst, asRegister(dstValue));
+        masm.mov(32, len, asRegister(lenValue));
 
-        // return immediately if length is zero
-        masm.cbz(32, result, done);
-
-        Register srcAddress = asRegister(temp1);
-        Register destAddress = asRegister(temp2);
-        masm.mov(64, srcAddress, asRegister(src));
-        masm.mov(64, destAddress, asRegister(dst));
-
-        masm.compare(64, result, CHUNK_ELEMENT_COUNT);
-        masm.branchConditionally(ConditionFlag.GE, simdImpl);
-
-        masm.bind(scalarImpl);
-        emitScalar(masm, done, srcAddress, destAddress, result);
-        masm.jmp(done);
-
-        masm.bind(simdImpl);
-        emitSIMD(masm, scalarImpl, done, srcAddress, destAddress, result);
-
-        masm.bind(done);
-    }
-
-    private static void emitScalar(AArch64MacroAssembler masm, Label done, Register srcAddress, Register destAddress, Register result) {
-        Label failToCompress = new Label();
-        Label scalarLoop = new Label();
-
-        try (ScratchRegister scratchReg1 = masm.getScratchRegister(); ScratchRegister scratchReg2 = masm.getScratchRegister()) {
-            Register val = scratchReg1.getRegister();
-            Register count = scratchReg2.getRegister();
-
-            masm.mov(64, count, result);
-
-            masm.align(AArch64MacroAssembler.PREFERRED_LOOP_ALIGNMENT);
-            masm.bind(scalarLoop);
-            masm.ldr(16, val, AArch64Address.createImmediateAddress(16, AddressingMode.IMMEDIATE_POST_INDEXED, srcAddress, 2));
-            /*
-             * If the current char is greater than 0xFF, stop compressing and return zero as result.
-             */
-            masm.compare(64, val, 0xFF);
-            masm.branchConditionally(ConditionFlag.GT, failToCompress);
-
-            masm.str(8, val, AArch64Address.createImmediateAddress(8, AddressingMode.IMMEDIATE_POST_INDEXED, destAddress, 1));
-            masm.subs(64, count, count, 1);
-            masm.branchConditionally(ConditionFlag.GT, scalarLoop);
-            masm.jmp(done);
-
-            masm.bind(failToCompress);
-            masm.mov(64, result, zr);
+        AArch64EncodeArrayOp.emitEncodeArrayOp(masm, res, src, dst, len, vectorTemp, LIRGeneratorTool.CharsetName.ISO_8859_1);
+        if (JavaVersionUtil.JAVA_SPEC < 22) {
+            // legacy behavior: if (result != length) { result = 0; }
+            masm.cmp(32, res, asRegister(lenValue));
+            masm.csel(32, res, res, zr, AArch64Assembler.ConditionFlag.EQ);
         }
-    }
-
-    private void emitSIMD(AArch64MacroAssembler masm, Label scalarImpl, Label done, Register srcChunkAddress, Register destChunkAddress, Register result) {
-        Register chunkPart1RegV = asRegister(vectorTemp1);
-        Register chunkPart2RegV = asRegister(vectorTemp2);
-        Register tmpRegV1 = asRegister(vectorTemp3);
-        Register tmp = asRegister(temp3);
-
-        Label simdLoop = new Label();
-        Label failToCompress = new Label();
-        Label redoEntireChunk = new Label();
-
-        try (ScratchRegister scratchRegister1 = masm.getScratchRegister(); ScratchRegister scratchRegister2 = masm.getScratchRegister()) {
-            Register lastChunkAddress = scratchRegister1.getRegister();
-            Register endOfSrcAddress = scratchRegister2.getRegister();
-            masm.add(64, endOfSrcAddress, srcChunkAddress, result, AArch64Assembler.ShiftType.LSL, 1);
-            masm.sub(64, lastChunkAddress, endOfSrcAddress, CHUNK_ELEMENT_COUNT * 2);
-
-            masm.align(AArch64MacroAssembler.PREFERRED_LOOP_ALIGNMENT);
-            masm.bind(simdLoop);
-            masm.fldp(128, chunkPart1RegV, chunkPart2RegV, AArch64Address.createImmediateAddress(128, AddressingMode.IMMEDIATE_PAIR_POST_INDEXED, srcChunkAddress, CHUNK_ELEMENT_COUNT * 2));
-            /*
-             * If an element in the chunk is greater than 0xFF, stop compressing and retry with
-             * scalar code.
-             *
-             * To do so, we check for any top bytes which are not zero.
-             */
-            masm.neon.orrVVV(AArch64ASIMDAssembler.ASIMDSize.FullReg, tmpRegV1, chunkPart1RegV, chunkPart2RegV);
-            masm.neon.uzp2VVV(AArch64ASIMDAssembler.ASIMDSize.FullReg, ElementSize.Byte, tmpRegV1, tmpRegV1, tmpRegV1);
-            masm.neon.umovGX(ElementSize.DoubleWord, tmp, tmpRegV1, 0);
-            masm.cbnz(64, tmp, failToCompress);
-
-            // compress elements
-            masm.neon.xtnVV(ElementSize.Byte, tmpRegV1, chunkPart1RegV);
-            masm.neon.xtn2VV(ElementSize.Byte, tmpRegV1, chunkPart2RegV);
-            masm.fstr(128, tmpRegV1, AArch64Address.createImmediateAddress(128, AddressingMode.IMMEDIATE_POST_INDEXED, destChunkAddress, CHUNK_ELEMENT_COUNT));
-            masm.cmp(64, srcChunkAddress, lastChunkAddress);
-            masm.branchConditionally(ConditionFlag.LO, simdLoop);
-
-            /*
-             * Process the last chunk. Move the source position back to the last chunk, 16 bytes
-             * before the end of the input array. Move the destination position back twice the
-             * movement of source position.
-             */
-            masm.cmp(64, srcChunkAddress, endOfSrcAddress);
-            masm.branchConditionally(ConditionFlag.HS, done);
-            masm.sub(64, srcChunkAddress, srcChunkAddress, lastChunkAddress);
-            // srcChunkAddress will be a value in the range (1, 31), so LSR and ASR are equivalent
-            masm.sub(64, destChunkAddress, destChunkAddress, srcChunkAddress, AArch64Assembler.ShiftType.LSR, 1);
-            masm.mov(64, srcChunkAddress, lastChunkAddress);
-            masm.jmp(simdLoop);
-        }
-
-        masm.bind(failToCompress);
-        /*
-         * Determine whether the first un-compressible element was in the first chunk. Compress and
-         * copy the first 16 bytes if the first un-compressible chars is in the second chunk.
-         * Afterwards, jump to the scalar loop to process the failed chunk element-by-element.
-         *
-         * Because we start immediately at the failing chunk part, we know at most 8 elements need
-         * to be checked to find the mismatch.
-         */
-        masm.mov(result, 8);
-        masm.neon.uzp2VVV(AArch64ASIMDAssembler.ASIMDSize.FullReg, ElementSize.Byte, tmpRegV1, chunkPart1RegV, chunkPart1RegV);
-        masm.neon.umovGX(ElementSize.DoubleWord, tmp, tmpRegV1, 0);
-        masm.cbnz(64, tmp, redoEntireChunk);
-
-        // fall-through: store chunkPart1 and start search at chunkPart2
-        // note this is really CHUNK_ELEMENT_SIZE * 2 / 2
-        masm.sub(64, srcChunkAddress, srcChunkAddress, CHUNK_ELEMENT_COUNT);
-        masm.neon.xtnVV(ElementSize.Byte, tmpRegV1, chunkPart1RegV);
-        masm.fstr(64, tmpRegV1, AArch64Address.createImmediateAddress(64, AddressingMode.IMMEDIATE_POST_INDEXED, destChunkAddress, CHUNK_ELEMENT_COUNT / 2));
-        masm.jmp(scalarImpl);
-
-        // start at chunkPart1
-        masm.bind(redoEntireChunk);
-        masm.sub(64, srcChunkAddress, srcChunkAddress, CHUNK_ELEMENT_COUNT * 2);
-        masm.jmp(scalarImpl);
     }
 }